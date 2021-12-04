7c95f53fc73c7b4e5ccd1115c2eed37fd6651524
Courtney Brown <courtnhey.brown@gmail.com>
2021-12-04 18:08:01 -0500

    minor changes

--- scripts.js
+++ scripts.js
@@ -1,41 +1,31 @@
 window.onload = function(){
-    bugme()
+
+    bugme() 
+	
 };
 
 function bugme(){
 
+
+
 	let button = document.querySelector('form');
     let login = button.getElementsByTagName('button')[0];
-	let issues = document.getElementsByClassName("issues")
-    let new_issue = document.getElementsByClassName("new_issue")
-	let add_user = document.getElementsByClassName("add_user");
-	
-	console.log(login);
-	function loginuser(){
-		let user = document.getElementById('login_username').value;
-		let passw = document.getElementById('login_password').value;
-		console.log(user);
-		console.log(passw);
-		console.log(new_issue);
-		console.log(add_user);
-	
-		console.log(issues);
 
-	issues.id ="unhidden";
-	console.log(issues);
-
-
+	function loginUser(){
+		$("jobdetails").setAttribute("id", "unhide");
 	}
-
-
+	
+	
 	login.addEventListener('click', (event) => {
-		
-		event.preventDefault();
-        loginuser();
-		
+        loginUser();
+        event.preventDefault();
         
+    })   
+
+	
+
+
         
-    })
 
 
 
