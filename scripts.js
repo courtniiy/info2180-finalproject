ae687889d10883f82366252d42823155061b25a7
Courtney Brown <courtnhey.brown@gmail.com>
2021-12-04 23:28:55 -0500

    minor changes

--- scripts.js
+++ scripts.js
@@ -8,46 +8,92 @@ function bugme(){
 
 
 	let logged_in = false;
-	let button = document.querySelector('form');
-    let login = button.getElementsByTagName('button')[0];
-	let issues= document.getElementsByClassName("issues");
 
 
-	function show_many(a){
-		for (let element of a) { 
-			element.show();		
+	function empty(txt) 
+	{
+	 if (txt.length === 0)
+	   {   
+		  return true; 
+	   }  	
+	   else{
+		   return false; 
+	 } }
+ 
+	 function validateEmail(email){
+		 let regex = /\S+@\S+\.\S+/;
+		 return regex.test(email);
+				 }
+ 
+	 function validPassword(pword){
+		 let regex =	/^(?=.*\d)(?=.*[A-Z])(?!.*[^a-zA-Z0-9@#$^+=])(.{8,15})$/;
+		 return regex.test(pword);
+ 
+	 }
+	
+
+
+	$( "#userLogin" ).click(function() {
+
+		if (empty($("#login_username").text())){
+			alert("Email Address field is emty");
 		}
-	}
 
-	function loginUser(){
+		else if (validateEmail($("#login_username").text())== false){
+			alert("Please enter a valid email address");
+		}
+
+		else if (empty($("#login_password").text())){
+			alert("Password field is emty");
+		}
+
+		else if (validPassword($("#login_password").text()) == false){
+			alert("Password not valid!");
+		}
+
+		else{
+
+			/*if function to verify login details */
+			logged_in = true;
+			$(".login").hide();
+			$('.issues').show();
+
+			/*else alert("Invalid login details!") */
+		}
+
 		
-		show_many(issues);
-		$(".login").hide();
-		logged_in = true;
-			
-	}
-
-	login.addEventListener('click', (event) => {
-		event.preventDefault();
-        loginUser();
-        
-        
-    }) 
-
+		
+		
+	  
+	});
 
 
 	$( ".log-out" ).click(function() {
-			$("#issues").hide();
-			$(".login").show();
+			if (logged_in == true){
 			logged_in = false;
-			console.log($('.login'));
+			$(".login").show();
+			$('.issues').hide();
+			$('.add_user').hide();
+			$('.new_issue').hide();
+
+			}
+
+			else{
+				alert("Already logged out");
+			}
+			
+			
 		  
 		});
 
-	$('home_btn').click(function() {
+	$('.home_btn').click(function() {
 
 		if (logged_in==true){
 			$('.issues').show();
+			$('.add_user').hide();
+			$('.new_issue').hide();
+			$('.login').hide();
+			
 		}
 
 		else{
@@ -59,6 +105,10 @@ function bugme(){
 
 		if (logged_in==true){
 			$('.add_user').show();
+			$('.new_issue').hide();
+			$('.login').hide();
+			$('.issues').hide();
+			
 		}
 
 		else{
@@ -71,12 +121,58 @@ function bugme(){
 
 		if (logged_in==true){
 			$('.new_issue').show();
+			$('.login').hide();
+			$('.issues').hide();
+			$('.add_user').hide();
+
 		}
 
 		else{
 			alert("Must be logged in to create new issues!")
 		}
 	});
+
+
+
+
+				
+
+	$("add_user_submit").click(function() {
+
+		if (empty($("#adduserfirstname").text())){
+			alert("First name field is emty");
+		}
+
+		else if (empty($("#adduserlastname").text())){
+			alert("Last name field is emty");
+		}
+		else if (empty($("#adduseremail").text())){
+				alert("Email Address field is emty");
+			}
+
+		else if (validateEmail($("#adduseremail").text())== false){
+				alert("Please enter a valid email address");
+			}
+
+		else if (empty($("#adduserpassword").text())){
+				alert("Password field is emty");
+			}
+
+		else if (validPassword($("#adduserpassword").text()) == false){
+				alert("Password not valid!");
+			}
+
+		else{
+
+			let new_user = [$("#adduserfirstname").text(), $("#adduserlastname").text(), $("#adduseremail").text()
+			("#adduserpassword").text()	];
+
+			/*function to add user to database */
+		}
+		
+				});
+
+
 
 		
 	
