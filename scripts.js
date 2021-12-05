1743ac436f671424d4ad99521b74cbff019832ef
Courtney Brown <courtnhey.brown@gmail.com>
2021-12-04 22:18:01 -0500

    Changes to index.html,sripts.js and styles.css

--- scripts.js
+++ scripts.js
@@ -7,28 +7,82 @@ window.onload = function(){
 function bugme(){
 
 
-
+	let logged_in = false;
 	let button = document.querySelector('form');
     let login = button.getElementsByTagName('button')[0];
+	let issues= document.getElementsByClassName("issues");
+
+
+	function show_many(a){
+		for (let element of a) { 
+			element.show();		
+		}
+	}
 
 	function loginUser(){
-		if (/*funtion to verify password*/ == true){
-			$(".issues").show();
-			$(".login").hide();
-		}
+		
+		show_many(issues);
+		$(".login").hide();
+		logged_in = true;
+			
+	}
 
-		else{
-			$("#loginAlert").innerHTML("Password is incorrect!");
-	}
-	}
-	
-	
 	login.addEventListener('click', (event) => {
 		event.preventDefault();
         loginUser();
         
         
-    })   
+    }) 
+
+
+
+	$( ".log-out" ).click(function() {
+			$("#issues").hide();
+			$(".login").show();
+			logged_in = false;
+			console.log($('.login'));
+		  
+		});
+
+	$('home_btn').click(function() {
+
+		if (logged_in==true){
+			$('.issues').show();
+		}
+
+		else{
+			alert("Must be logged in to view issues!")
+		}
+	});
+
+	$('.add_user_home').click(function() {
+
+		if (logged_in==true){
+			$('.add_user').show();
+		}
+
+		else{
+			alert("Must be logged in to add users!")
+		}
+	});
+
+
+	$('.new_issue_home').click(function() {
+
+		if (logged_in==true){
+			$('.new_issue').show();
+		}
+
+		else{
+			alert("Must be logged in to create new issues!")
+		}
+	});
+
+		
+	
+	
+	
+	
 
 	
 
